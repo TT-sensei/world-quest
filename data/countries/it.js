6bd0d9c41ace24b59b02451e610e456691cdeb47
TT-sensei <rlrieaeoy@gmail.com>
2026-09-21 00:12:04 +0900

    イタリアの画像リンクを確認・修正

--- data/countries/it.js
+++ data/countries/it.js
@@ -1,1 +1,24 @@
-window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];window.WORLD_COUNTRIES.push({id:"it",name:"イタリア",code:"IT",region:"南ヨーロッパ",continent:"ヨーロッパ",capital:"ローマ",flag:"assets/flags/it.svg",emoji:"🇮🇹",items:{place:[{name:"ピサの斜塔",kind:"名所",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Tower_of_Pisa_January_2013.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Tower_of_Pisa_January_2013.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Not sparse"},{name:"ミラノ大聖堂(ドゥオーモ)",kind:"名所",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Milan_Cathedral_from_Piazza_del_Duomo.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Milan_Cathedral_from_Piazza_del_Duomo.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Jiuguang Wang"}],heritage:[{name:"コロッセオ(ローマ歴史地区)",kind:"世界遺産",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Colosseum_in_Rome%2C_Italy_-_March_2007.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Colosseum_in_Rome,_Italy_-_March_2007.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"DAVID ILIFF"}],culture:[{name:"ベネチアのカーニバル",kind:"伝統・文化",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Carnevale_di_Venezia_2010.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Carnevale_di_Venezia_2010.jpg",source:"Wikimedia Commons",license:"CC BY 2.0",credit:"Frank Kovalchek"}],food:[{name:"ピッツァ・マルゲリータ",kind:"食文化",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Pizza_Margherita_01.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Pizza_Margherita_01.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Valerio Capello"}],facility:[],nature:[{name:"青の洞窟(カプリ島)",kind:"自然",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Grotta_Azzurra_Capri.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Grotta_Azzurra_Capri.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Signorfabbro"}],symbol:[{name:"ヴェネツィアのゴンドラ",kind:"シンボル",image:"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/Gondolas_in_Venice.jpg&width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Gondolas_in_Venice.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Sillerkiil"}]}});
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];window.WORLD_COUNTRIES.push({
+id:"it",name:"イタリア",code:"IT",region:"南ヨーロッパ",continent:"ヨーロッパ",capital:"ローマ",flag:"assets/flags/it.svg",emoji:"🇮🇹",
+items:{
+place:[
+{name:"ピサの斜塔",kind:"名所",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Italy%20-%20Pisa%20-%20Leaning%20Tower%20of%20Pisa.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Italy_-_Pisa_-_Leaning_Tower_of_Pisa.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Arne Müseler"},
+{name:"ミラノ大聖堂(ドゥオーモ)",kind:"名所",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Milan%20Cathedral%20from%20Piazza%20del%20Duomo.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Milan_Cathedral_from_Piazza_del_Duomo.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Jiuguang Wang"}
+],
+heritage:[
+{name:"コロッセオ(ローマ歴史地区)",kind:"世界遺産",image:"https://commons.wikimedia.org/wiki/Special:FilePath/The%20Colosseum%20in%20Rome%2C%202007.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:The_Colosseum_in_Rome,_2007.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"МаратД"}
+],
+culture:[
+{name:"ベネチアのカーニバル",kind:"伝統・文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Carnevale%20di%20Venezia%20-%202010%20%284358421466%29.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Carnevale_di_Venezia_-_2010_(4358421466).jpg",source:"Wikimedia Commons",license:"CC BY-SA 2.0",credit:"Giorgio Minguzzi"}
+],
+food:[
+{name:"ピッツァ・マルゲリータ",kind:"食文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Pizza%20Margherita%2001.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Pizza_Margherita_01.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Arnaud 25"}
+],
+facility:[],
+nature:[
+{name:"青の洞窟(カプリ島)",kind:"自然",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Blue%20Grotto%20Capri%20Inside.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Blue_Grotto_Capri_Inside.jpg",source:"Wikimedia Commons",license:"CC BY-SA 1.0",credit:"Arnaud Gaillard"}
+],
+symbol:[
+{name:"ヴェネツィアのゴンドラ",kind:"シンボル",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Gondolas%20in%20Venice.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Gondolas_in_Venice.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Jsamwrites"}
+]
+}});
