011f9ddbf3504cd54a16118fccb5b97f2588a882
TT-sensei <rlrieaeoy@gmail.com>
2026-09-21 00:39:59 +0900

    フランスを追加

---
+++ data/countries/fr.js
@@ -0,0 +1,11 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];window.WORLD_COUNTRIES.push({
+id:"fr",name:"フランス",code:"FR",region:"西ヨーロッパ",continent:"ユーラシア",capital:"パリ",flag:"assets/flags/fr.svg",emoji:"🇫🇷",
+items:{
+place:[{name:"エッフェル塔",kind:"名所",image:"https://commons.wikimedia.org/wiki/Special:FilePath/The_Eiffel_Tower_in_Paris%2C_France.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:The_Eiffel_Tower_in_Paris,_France.jpg",source:"Wikimedia Commons"}],
+heritage:[{name:"モン・サン＝ミシェル",kind:"世界遺産",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Mont_Saint-Michel_3.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Mont_Saint-Michel_3.jpg",source:"Wikimedia Commons"}],
+culture:[{name:"フランスのバレエ",kind:"伝統・文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ballet_dancers.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Ballet_dancers.jpg",source:"Wikimedia Commons"}],
+food:[{name:"クロワッサン",kind:"食文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Croissant-Petr_Kratochvil.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Croissant-Petr_Kratochvil.jpg",source:"Wikimedia Commons"}],
+facility:[{name:"ルーヴル美術館",kind:"施設",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Louvre_Museum_Wikimedia_Commons.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Louvre_Museum_Wikimedia_Commons.jpg",source:"Wikimedia Commons"}],
+nature:[{name:"アルプス山脈",kind:"自然",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Mont_Blanc_from_Planpraz.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Mont_Blanc_from_Planpraz.jpg",source:"Wikimedia Commons"}],
+symbol:[{name:"ニワトリ(フランスのシンボル)",kind:"シンボル",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Gallus_gallus_domesticus.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Gallus_gallus_domesticus.jpg",source:"Wikimedia Commons"}]
+}});
