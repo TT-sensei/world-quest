c800bcd17e590f63d70267ca16aacd817b3ffff6
TT-sensei <rlrieaeoy@gmail.com>
2026-09-21 06:49:17 +0900

    fix: replace broken France image URLs

--- data/countries/fr.js
+++ data/countries/fr.js
@@ -3,9 +3,9 @@ id:"fr",name:"フランス",code:"FR",region:"西ヨーロッパ",continent:"ユ
 items:{
 place:[{name:"エッフェル塔",kind:"名所",image:"https://commons.wikimedia.org/wiki/Special:FilePath/The_Eiffel_Tower_in_Paris%2C_France.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:The_Eiffel_Tower_in_Paris,_France.jpg",source:"Wikimedia Commons"}],
 heritage:[{name:"モン・サン＝ミシェル",kind:"世界遺産",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Mont_Saint-Michel_3.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Mont_Saint-Michel_3.jpg",source:"Wikimedia Commons"}],
-culture:[{name:"フランスのバレエ",kind:"伝統・文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ballet_dancers.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Ballet_dancers.jpg",source:"Wikimedia Commons"}],
+culture:[{name:"フランスのバレエ",kind:"伝統・文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/The_ballet_dancers.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:The_ballet_dancers.jpg",source:"Wikimedia Commons"}],
 food:[{name:"クロワッサン",kind:"食文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Croissant-Petr_Kratochvil.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Croissant-Petr_Kratochvil.jpg",source:"Wikimedia Commons"}],
 facility:[{name:"ルーヴル美術館",kind:"施設",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Louvre_Museum_Wikimedia_Commons.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Louvre_Museum_Wikimedia_Commons.jpg",source:"Wikimedia Commons"}],
-nature:[{name:"アルプス山脈",kind:"自然",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Mont_Blanc_from_Planpraz.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Mont_Blanc_from_Planpraz.jpg",source:"Wikimedia Commons"}],
+nature:[{name:"アルプス山脈",kind:"自然",image:"https://commons.wikimedia.org/wiki/Special:FilePath/French_alps.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:French_alps.jpg",source:"Wikimedia Commons"}],
 symbol:[{name:"ガリアの雄鶏(フランスの象徴)",kind:"シンボル",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Monument_aux_Girondins_-_Coq_gaulois.JPG?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Monument_aux_Girondins_-_Coq_gaulois.JPG",source:"Wikimedia Commons",license:"GFDL",credit:"Ophelia2"}]
 }});
