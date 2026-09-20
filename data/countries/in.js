28d495f321ae1e6abe40faa5e9ea270dd647f236
TT-sensei <rlrieaeoy@gmail.com>
2026-09-21 00:38:39 +0900

    インドを追加

---
+++ data/countries/in.js
@@ -0,0 +1,11 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];window.WORLD_COUNTRIES.push({
+id:"in",name:"インド",code:"IN",region:"南アジア",continent:"ユーラシア",capital:"ニューデリー",flag:"assets/flags/in.svg",emoji:"🇮🇳",
+items:{
+place:[{name:"タージ・マハル",kind:"名所",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Taj_Mahal%2C_Agra%2C_India.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Taj_Mahal,_Agra,_India.jpg",source:"Wikimedia Commons"}],
+heritage:[{name:"アジャンター石窟群",kind:"世界遺産",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ajanta_Caves%2C_India.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Ajanta_Caves,_India.jpg",source:"Wikimedia Commons"}],
+culture:[{name:"ホーリー祭",kind:"伝統・文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Holi_celebration.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Holi_celebration.jpg",source:"Wikimedia Commons"}],
+food:[{name:"ナンとカレー",kind:"食文化",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Naan_bread.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Naan_bread.jpg",source:"Wikimedia Commons"}],
+facility:[{name:"インド門",kind:"施設",image:"https://commons.wikimedia.org/wiki/Special:FilePath/India_Gate_in_New_Delhi_at_Night.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:India_Gate_in_New_Delhi_at_Night.jpg",source:"Wikimedia Commons"}],
+nature:[{name:"ガンジス川",kind:"自然",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Ganges_River_in_Varanasi.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Ganges_River_in_Varanasi.jpg",source:"Wikimedia Commons"}],
+symbol:[{name:"インドゾウ",kind:"シンボル動物",image:"https://commons.wikimedia.org/wiki/Special:FilePath/Indian_elephant.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Indian_elephant.jpg",source:"Wikimedia Commons"}]
+}});
