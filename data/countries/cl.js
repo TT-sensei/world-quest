5399db31c29100c7e5b3ea7cc0c7b9bf033f2415
TT-sensei <rlrieaeoy@gmail.com>
2026-09-21 23:44:13 +0900

    Add Chile to world country encyclopedia

---
+++ data/countries/cl.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"cl",
+  name:"チリ",
+  code:"CL",
+  region:"南アメリカ",
+  continent:"南アメリカ",
+  capital:"サンティアゴ",
+  flag:"assets/flags/cl.svg",
+  emoji:"🇨🇱",
+  items:{
+    place:[
+      {
+        name:"セロ・アレグレの坂の町並み(バルパライソ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Cerro%20Alegre%2C%20Valpara%C3%ADso%2C%20Chile2.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Cerro_Alegre,_Valpara%C3%ADso,_Chile2.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Poco a poco"
+      }
+    ],
+    heritage:[
+      {
+        name:"モアイ像(イースター島・ラパヌイ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Moai%20Rano%20raraku.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Moai_Rano_raraku.jpg",
+        source:"Wikimedia Commons",
+        license:"パブリックドメイン",
+        credit:"Aurbina"
+      }
+    ],
+    culture:[
+      {
+        name:"クエッカ(チリの国の踊り)の伝統衣装",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Traditional%20dress%20of%20the%20Chilean%20cueca%2C%20a%20national%20dance.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Traditional_dress_of_the_Chilean_cueca,_a_national_dance.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Quilicura"
+      }
+    ],
+    food:[
+      {
+        name:"エンパナーダ(ポマイレの郷土料理)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Empanada%20t%C3%ADpica%20de%20Pomaire%2C%20Chile%20-%20A741218.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Empanada_t%C3%ADpica_de_Pomaire,_Chile_-_A741218.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Rjcastillo"
+      }
+    ],
+    facility:[
+      {
+        name:"バブリッザ宮殿(バルパライソ美術館)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/PALACIO%20BABURIZZA.JPG?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:PALACIO_BABURIZZA.JPG",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Juncal2014"
+      }
+    ],
+    nature:[
+      {
+        name:"トレス・デル・パイネ国立公園の山々(パタゴニア)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Cuernos%20del%20Paine%2C%20Parque%20Nacional%20Torres%20del%20Paine%2C%20Chile7.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Cuernos_del_Paine,_Parque_Nacional_Torres_del_Paine,_Chile7.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Poco a poco"
+      }
+    ],
+    symbol:[
+      {
+        name:"コピウエ(チリの国花)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Lapageria%20rosea%20-%20Copihue.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Lapageria_rosea_-_Copihue.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 2.0",
+        credit:"Inao Vásquez"
+      }
+    ]
+  }
+});
