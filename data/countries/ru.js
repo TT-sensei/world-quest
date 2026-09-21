11e856c23fecf4d0cdd8d31b8f18897b9744d719
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 06:35:03 +0900

    Add Russia country data

---
+++ data/countries/ru.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"ru",
+  name:"ロシア",
+  code:"RU",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"モスクワ",
+  flag:"assets/flags/ru.svg",
+  emoji:"🇷🇺",
+  items:{
+    place:[
+      {
+        name:"赤の広場とクレムリン(モスクヴォレツキー橋から)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/The%20Moscow%20Kremlin%20and%20Red%20Square%20as%20seen%20from%20Bolshoy%20Moskvoretsky%20Bridge%2C%20Moscow%2C%20Russia.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:The_Moscow_Kremlin_and_Red_Square_as_seen_from_Bolshoy_Moskvoretsky_Bridge,_Moscow,_Russia.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 4.0",
+        credit:"Vyacheslav Argenberg"
+      }
+    ],
+    heritage:[
+      {
+        name:"聖ワシリイ大聖堂(赤の広場)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Saint%20Basil%27s%20Cathedral%2C%20Red%20Square%2C%20Moscow%2C%20Russia.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Saint_Basil's_Cathedral,_Red_Square,_Moscow,_Russia.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 4.0",
+        credit:"Vyacheslav Argenberg"
+      }
+    ],
+    culture:[
+      {
+        name:"ロシアの民族衣装(サラファンとココシュニク)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Traditional%20Russian%20Folk%20Costumes%2002.JPG?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Traditional_Russian_Folk_Costumes_02.JPG",
+        source:"Wikimedia Commons",
+        license:"CC BY 4.0",
+        credit:"Ninaras"
+      }
+    ],
+    food:[
+      {
+        name:"ペリメニ(ロシアで親しまれる水ぎょうざ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Russian%20Pelmeni%20%28Dumplings%29%2C%20Rostov-on-Don%2C%20Russia.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Russian_Pelmeni_(Dumplings),_Rostov-on-Don,_Russia.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 4.0",
+        credit:"Vyacheslav Argenberg"
+      }
+    ],
+    facility:[
+      {
+        name:"ボリショイ劇場(モスクワ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Moscow%20-%202025%20-%20Facade%20of%20Big%20Theatre%20(1).jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Moscow_-_2025_-_Facade_of_Big_Theatre_(1).jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 4.0",
+        credit:"Юрий Д.К."
+      }
+    ],
+    nature:[
+      {
+        name:"バイカル湖 ブルハン岬(オリホン島)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Baikal%2C%20Cape%20Burhan%2C%20Olkhon%20Island%2C%20Lake%20Baikal%2C%20Russia.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Baikal,_Cape_Burhan,_Olkhon_Island,_Lake_Baikal,_Russia.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 4.0",
+        credit:"Vyacheslav Argenberg"
+      }
+    ],
+    symbol:[
+      {
+        name:"シラカバの林(ロシアを代表する樹木)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Birch%20bog20060915%20083.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Birch_bog20060915_083.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Bff"
+      }
+    ]
+  }
+});
