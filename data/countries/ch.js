f92c37d2551eb812e2b91f4be0ba88c4e3fac2a8
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 11:53:36 +0900

    Add Switzerland country data

---
+++ data/countries/ch.js
@@ -0,0 +1,69 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"ch",
+  name:"スイス",
+  code:"CH",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"ベルン",
+  flag:"assets/flags/ch.svg",
+  emoji:"🇨🇭",
+  items:{
+    place:[{
+      name:"カペル橋(ルツェルン)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Chapel%20Bridge%20(Kapellbr%C3%BCcke)%2C%20Lucerne%2C%20Switzerland%2002.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Chapel_Bridge_(Kapellbr%C3%BCcke),_Lucerne,_Switzerland_02.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Godot13"
+    }],
+    heritage:[{
+      name:"ツィットグロッゲ(時計塔・旧市街)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bern%20Zytglogge%20DSC05181.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Bern_Zytglogge_DSC05181.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 3.0",
+      credit:"Wikimedia Commons(Wiki Loves Monuments 2011)"
+    }],
+    culture:[{
+      name:"アルプホルンの演奏",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Alphorns%20Grindelwald.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Alphorns_Grindelwald.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 3.0",
+      credit:"Wikimedia Commons contributor"
+    }],
+    food:[{
+      name:"チーズフォンデュ",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Cheese%20fondue-01-a.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Cheese_fondue-01-a.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY 2.0",
+      credit:"Jocian"
+    }],
+    facility:[{
+      name:"シヨン城(レマン湖畔)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Ch%C3%A2teau%20de%20Chillon(Chillon%20Castle)%20on%20Lake%20Geneva%20Shore%20(Ank%20Kumar%2C%20Infosys)%2006.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Ch%C3%A2teau_de_Chillon(Chillon_Castle)_on_Lake_Geneva_Shore_(Ank_Kumar,_Infosys)_06.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Ank Kumar"
+    }],
+    nature:[{
+      name:"マッターホルン(ツェルマット)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Matterhorn%20as%20seen%20from%20Zermatt%2C%20Wallis%2C%20Switzerland%2C%202012%20August.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Matterhorn_as_seen_from_Zermatt,_Wallis,_Switzerland,_2012_August.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Ximonic"
+    }],
+    symbol:[{
+      name:"エーデルワイス(高山植物)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Edelweiss%20im%20Schweizer%20Nationalpark.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Edelweiss_im_Schweizer_Nationalpark.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Wikimedia Commons(Wiki Loves Switzerland 2020)"
+    }]
+  }
+});
