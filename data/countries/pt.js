aa85b8d5853c541e220347766e96b99a93d2184b
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 11:57:00 +0900

    Add Portugal country data

---
+++ data/countries/pt.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"pt",
+  name:"ポルトガル",
+  code:"PT",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"リスボン",
+  flag:"assets/flags/pt.svg",
+  emoji:"🇵🇹",
+  items:{
+    place:[
+      {
+        name:"ドン・ルイス1世橋とリベイラ地区",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Ponte%20D%20Luis%20e%20ribeira%20do%20Porto.JPG?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Ponte_D_Luis_e_ribeira_do_Porto.JPG",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Béria Lima"
+      }
+    ],
+    heritage:[
+      {
+        name:"ベレンの塔",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Torre%20de%20Bel%C3%A9m%20-%20Bel%C3%A9m%20Tower%2C%20Lisbon%20(23332962505).jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Torre_de_Bel%C3%A9m_-_Bel%C3%A9m_Tower,_Lisbon_(23332962505).jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 2.0",
+        credit:"Matt Kieffer"
+      }
+    ],
+    culture:[
+      {
+        name:"アズレージョ(装飾タイル)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Azulejo%20-%20Igreja%20de%20S%C3%A3o%20Bento%20-%20Ribeira%20Brava.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Azulejo_-_Igreja_de_S%C3%A3o_Bento_-_Ribeira_Brava.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Llez"
+      }
+    ],
+    food:[
+      {
+        name:"エッグタルト(パステル・デ・ナタ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Pasteis%20de%20Nata%20001.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Pasteis_de_Nata_001.jpg",
+        source:"Wikimedia Commons",
+        license:"パブリックドメイン(CC0)",
+        credit:"Ocdp"
+      }
+    ],
+    facility:[
+      {
+        name:"ペーナ宮殿(シントラ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Pena%20Palace%20Sintra.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Pena_Palace_Sintra.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Sergey Pesterev"
+      }
+    ],
+    nature:[
+      {
+        name:"ドウロ川流域のぶどう畑",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Valen%C3%A7a%20do%20Douro.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Valen%C3%A7a_do_Douro.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Wikimedia Commons contributor"
+      }
+    ],
+    symbol:[
+      {
+        name:"バルセロスの雄鶏",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Galo%20de%20Barcelos%20png.png?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Galo_de_Barcelos_png.png",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Wikimedia Commons contributor"
+      }
+    ]
+  }
+});
