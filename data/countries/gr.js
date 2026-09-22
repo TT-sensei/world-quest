4675adefd8bfdaeae80b7bab2091df154af611d6
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 16:45:18 +0900

    Add Greece country data

---
+++ data/countries/gr.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"gr",
+  name:"ギリシャ",
+  code:"GR",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"アテネ",
+  flag:"assets/flags/gr.svg",
+  emoji:"🇬🇷",
+  items:{
+    place:[
+      {
+        name:"白い街並みと青いドーム屋根(サントリーニ島)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Oia%20Santorini%20Blue%20Domes.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Oia_Santorini_Blue_Domes.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Danbu14"
+      }
+    ],
+    heritage:[
+      {
+        name:"アクロポリス(アテネ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Attica%2006-13%20Athens%2050%20View%20from%20Philopappos%20-%20Acropolis%20Hill.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Attica_06-13_Athens_50_View_from_Philopappos_-_Acropolis_Hill.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Wikimedia Commons(Featured picture)"
+      }
+    ],
+    culture:[
+      {
+        name:"ゼウス像(考古学博物館)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Marble%20statue%20of%20Zeus%20in%20the%20Archaeological%20Museum%20of%20Olympia%2C%20Greece%20(51223275498).jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Marble_statue_of_Zeus_in_the_Archaeological_Museum_of_Olympia,_Greece_(51223275498).jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 2.0",
+        credit:"dronepicr"
+      }
+    ],
+    food:[
+      {
+        name:"ムサカ",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Moussaka%2012.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Moussaka_12.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 2.0",
+        credit:"Wikimedia Commons contributor"
+      }
+    ],
+    facility:[
+      {
+        name:"パルテノン神殿",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Parthenon%20from%20south.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Parthenon_from_south.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 2.5",
+        credit:"Thermos"
+      }
+    ],
+    nature:[
+      {
+        name:"エーゲ海の眺め",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Aegean%20sea.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Aegean_sea.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"WindEwriX"
+      }
+    ],
+    symbol:[
+      {
+        name:"オリーブの古木",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Ancient%20olive%20tree%20in%20Elia%20village%2C%20Lakonia%2C%20Greece%20(1).JPG?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Ancient_olive_tree_in_Elia_village,_Lakonia,_Greece_(1).JPG",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Stratis Stamatakos"
+      }
+    ]
+  }
+});
