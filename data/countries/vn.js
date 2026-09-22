b512886bd924140c217cd52a7f6478f5a7f1dc06
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 11:50:21 +0900

    feat: ベトナムを国図鑑に追加

---
+++ data/countries/vn.js
@@ -0,0 +1,55 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"vn",
+  name:"ベトナム",
+  code:"VN",
+  region:"アジア",
+  continent:"ユーラシア",
+  capital:"ハノイ",
+  flag:"assets/flags/vn.svg",
+  emoji:"🇻🇳",
+  items:{
+    place:[{
+      name:"ホイアン旧市街のランタン",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/H%E1%BB%99i%20An%2C%20Ancient%20Town%2C%202020-01%20CN-06.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:H%E1%BB%99i_An,_Ancient_Town,_2020-01_CN-06.jpg",
+      source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Carschten"
+    }],
+    heritage:[{
+      name:"ハロン湾の奇岩群",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Ha%20Long%20Bay%2C%20Vietnam%2C%20Rock%20formations.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Ha_Long_Bay,_Vietnam,_Rock_formations.jpg",
+      source:"Wikimedia Commons",license:"CC BY 4.0",credit:"Vyacheslav Argenberg"
+    }],
+    culture:[{
+      name:"アオザイ(伝統衣装)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/White%20Ao%20Dai%20-%208431%20-%20FLICKR.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:White_Ao_Dai_-_8431_-_FLICKR.jpg",
+      source:"Wikimedia Commons",license:"CC BY-SA 2.0",credit:"Phat Truong"
+    }],
+    food:[{
+      name:"フォー",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Vietnamese%20Pho.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Vietnamese_Pho.jpg",
+      source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Nowforever"
+    }],
+    facility:[{
+      name:"一柱寺(ハノイ)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/One%20Pillar%20Pagoda%20in%20Hanoi%2C%20Vietnam%20(25708058514).jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:One_Pillar_Pagoda_in_Hanoi,_Vietnam_(25708058514).jpg",
+      source:"Wikimedia Commons",license:"CC BY 2.0",credit:"Ltn12345"
+    }],
+    nature:[{
+      name:"サパの棚田",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Rice%20terraces%20in%20Sa%20Pa%2002.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Rice_terraces_in_Sa_Pa_02.jpg",
+      source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Christophe95"
+    }],
+    symbol:[{
+      name:"ハス(国花)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Lotus%20flower%20(978659).jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Lotus_flower_(978659).jpg",
+      source:"Wikimedia Commons",license:"パブリックドメイン(CC0)",credit:"Josve05a(Pixabay提供)"
+    }]
+  }
+});
