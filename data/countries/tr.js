bc487dce00d4e343618fd3b9f35d92b9275bc34d
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 11:54:52 +0900

    Add Turkey country data

---
+++ data/countries/tr.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"tr", name:"トルコ", code:"TR", region:"アジア", continent:"ユーラシア", capital:"アンカラ",
+  flag:"assets/flags/tr.svg", emoji:"🇹🇷",
+  items:{
+    place:[{name:"ガラタ塔からの眺め",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/View%20of%20Galata%20Tower%20from%20Fatih%2C%20Istanbul%2012.20.2023%2003.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:View_of_Galata_Tower_from_Fatih,_Istanbul_12.20.2023_03.jpg",source:"Wikimedia Commons",license:"CC BY 4.0",credit:"Wikimedia Commons contributor"}],
+    heritage:[{name:"アヤソフィア",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Istanbul%20asv2020-02%20img45%20Hagia%20Sophia.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Istanbul_asv2020-02_img45_Hagia_Sophia.jpg",source:"Wikimedia Commons",license:"FAL(自由芸術ライセンス)",credit:"A.Savin"}],
+    culture:[{name:"旋回舞踊(セマー)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Whirling%20Dervishes%202.JPG?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Whirling_Dervishes_2.JPG",source:"Wikimedia Commons",license:"CC BY-SA 2.0",credit:"Vladimer Shioshvili"}],
+    food:[{name:"ピスタチオのバクラヴァ",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/F%C4%B1st%C4%B1kl%C4%B1%20Baklava.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:F%C4%B1st%C4%B1kl%C4%B1_Baklava.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    facility:[{name:"ブルーモスク",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Blue%20Mosque%20Istanbul.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Blue_Mosque_Istanbul.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    nature:[{name:"奇岩と気球の風景(カッパドキア)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Cappadocia%20Aerial%20View%20Landscape.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Cappadocia_Aerial_View_Landscape.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons(Featured picture)"}],
+    symbol:[{name:"チューリップ(エミルガン公園)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Tulip%20Festival%20Emirgan%202018.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Tulip_Festival_Emirgan_2018.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons(Wiki Loves Earth 2020)"}]
+  }
+});
