67cd4f7946935419fc8c42d412f42ba74f2a7e35
TT-sensei <rlrieaeoy@gmail.com>
2026-09-22 16:47:05 +0900

    Add Singapore country data

---
+++ data/countries/sg.js
@@ -0,0 +1,20 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"sg",
+  name:"シンガポール",
+  code:"SG",
+  region:"アジア",
+  continent:"ユーラシア",
+  capital:"シンガポール",
+  flag:"assets/flags/sg.svg",
+  emoji:"🇸🇬",
+  items:{
+    place:[{name:"マリーナベイの夜景",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Marina%20Bay%20Singapore-3499.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Marina_Bay_Singapore-3499.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Bijay Chaurasia"}],
+    heritage:[{name:"バンドスタンド(植物園)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bandstand%2C%20Singapore%20Botanic%20Gardens%20-%2020060805.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Bandstand,_Singapore_Botanic_Gardens_-_20060805.jpg",source:"Wikimedia Commons",license:"CC BY-SA 2.5",credit:"Calvin Teo"}],
+    culture:[{name:"ディーパヴァリの灯り(リトル・インディア)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Deepavali%20on%20Serangoon%20Road.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Deepavali_on_Serangoon_Road.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Cpgfella"}],
+    food:[{name:"チキンライス",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Tian%20Tian%20Hainanese%20Chicken%20Rice%2C%20Singapore.JPG?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Tian_Tian_Hainanese_Chicken_Rice,_Singapore.JPG",source:"Wikimedia Commons",license:"CC BY-SA 1.0",credit:"Shoestring(Wikivoyage)"}],
+    facility:[{name:"マリーナベイ・サンズ",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Marina%20Bay%20Sands%20and%20illuminated%20polyhedral%20building%20Louis%20Vuitton%20over%20the%20water%20at%20blue%20hour%20with%20pink%20clouds%20in%20Singapore.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Marina_Bay_Sands_and_illuminated_polyhedral_building_Louis_Vuitton_over_the_water_at_blue_hour_with_pink_clouds_in_Singapore.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons(Featured picture)"}],
+    nature:[{name:"ガーデンズ・バイ・ザ・ベイ",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Singapore%20(SG)%2C%20Gardens%20By%20The%20Bay%20--%202019%20--%204725.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Singapore_(SG),_Gardens_By_The_Bay_--_2019_--_4725.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Dietmar Rabich"}],
+    symbol:[{name:"マーライオン像",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Merlion%20Statue%20at%20Merlion%20Park%2C%20Singapore%20(Ank%20Kumar%20)%2002.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Merlion_Statue_at_Merlion_Park,_Singapore_(Ank_Kumar_)_02.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Ank Kumar"}]
+  }
+});
