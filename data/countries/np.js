e39460dedcdaba1f3a61649b00b9ac89ca09cb34
TT-sensei <rlrieaeoy@gmail.com>
2026-09-24 05:57:00 +0900

    Add Nepal country data

---
+++ data/countries/np.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"np",name:"ネパール",code:"NP",region:"アジア",continent:"ユーラシア",capital:"カトマンズ",
+  flag:"assets/flags/np.svg",emoji:"🇳🇵",
+  items:{
+    place:[{name:"カトマンズ・ダルバール広場",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Kathmandu%20Durbar%20Square%2C%20Maju%20Dega%202%2C%20Nepal.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Kathmandu_Durbar_Square,_Maju_Dega_2,_Nepal.jpg",source:"Wikimedia Commons",license:"CC BY 4.0",credit:"Vyacheslav Argenberg"}],
+    heritage:[{name:"ボダナート・ストゥーパ",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Boudhanath%20stupa%2C%20Kathmandu%2001.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Boudhanath_stupa,_Kathmandu_01.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Bernard Gagnon"}],
+    culture:[{name:"ダサインの祭り(額にティカをつける風習)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Dashain%20Festival%20Nepal%2C%20Putting%20Tika%20in%20forehead%2010.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Dashain_Festival_Nepal,_Putting_Tika_in_forehead_10.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Nischal 61"}],
+    food:[{name:"モモ(蒸し餃子)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Momo%20Nepali%20cuisine.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Momo_Nepali_cuisine.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Bajinra"}],
+    facility:[{name:"ニャタポラ寺院(バクタプル)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Paanch%20Tale%20Temple%20(Nyatapola).jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Paanch_Tale_Temple_(Nyatapola).jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Nirajan pant"}],
+    nature:[{name:"夕焼けのエベレスト",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Sunset%20view%20of%20Everest.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Sunset_view_of_Everest.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Nir B. Gurung"}],
+    symbol:[{name:"ラリグラス(シャクナゲ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Rhododendron%20(LaliGurans)%20-%20National%20Flower%20of%20Nepal.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Rhododendron_(LaliGurans)_-_National_Flower_of_Nepal.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Saroj Pandey"}]
+  }
+});
