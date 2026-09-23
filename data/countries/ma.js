1fae4798147f089c4ce773b09aa05916786fa248
TT-sensei <rlrieaeoy@gmail.com>
2026-09-24 05:55:44 +0900

    Add Morocco country data

---
+++ data/countries/ma.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"ma",
+  name:"モロッコ",
+  code:"MA",
+  region:"アフリカ",
+  continent:"アフリカ",
+  capital:"ラバト",
+  flag:"assets/flags/ma.svg",
+  emoji:"🇲🇦",
+  items:{
+    place:[
+      {
+        name:"青い街並み(シャウエン)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Blue%20Town%20Chefchaouen.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Blue_Town_Chefchaouen.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Lin Wen Hung"
+      }
+    ],
+    heritage:[
+      {
+        name:"アイト・ベン・ハドゥの要塞集落",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/A%C3%AFt%20Benhaddou%20view%20from%20above.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:A%C3%AFt_Benhaddou_view_from_above.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Elena Tatiana Chis"
+      }
+    ],
+    culture:[
+      {
+        name:"ミントティーの喫茶文化",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Moroccan%20tea%201.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Moroccan_tea_1.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Ekaterina Kvelidze"
+      }
+    ],
+    food:[
+      {
+        name:"タジン鍋の鶏肉料理",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Moroccan%20food-Chicken%20tagine%20with%20preserved%20lemons%20and%20olives-01.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Moroccan_food-Chicken_tagine_with_preserved_lemons_and_olives-01.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 2.0",
+        credit:"Foodista"
+      }
+    ],
+    facility:[
+      {
+        name:"ハッサン2世モスク",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Sunrise%20in%20Casablanca%20with%20Hassan%20II%20Mosque.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Sunrise_in_Casablanca_with_Hassan_II_Mosque.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"SpreeTom"
+      }
+    ],
+    nature:[
+      {
+        name:"サハラ砂漠の砂丘(メルズーガ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Merzouga%20desert%20Erg%20Chebbi.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Merzouga_desert_Erg_Chebbi.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Elena Tatiana Chis"
+      }
+    ],
+    symbol:[
+      {
+        name:"ゼリージュ(幾何学模様のモザイクタイル)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/MOROCCAN%20TILES%20ZELLIGE.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:MOROCCAN_TILES_ZELLIGE.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Orlysi"
+      }
+    ]
+  }
+});
