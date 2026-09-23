3871f5e967433e2ae38c05de7c209691734dc140
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 12:42:23 +0900

    fiを国図鑑に追加

---
+++ data/countries/fi.js
@@ -0,0 +1,20 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"fi",
+  name:"フィンランド",
+  code:"FI",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"ヘルシンキ",
+  flag:"assets/flags/fi.svg",
+  emoji:"🇫🇮",
+  items:{
+    place:[{name:"ヘルシンキ大聖堂と元老院広場",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Helsinki%20Cathedral%20and%20Senate%20Square.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Helsinki_Cathedral_and_Senate_Square.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    heritage:[{name:"スオメンリンナ要塞の王の門",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/King%27s%20Gate%20Suomenlinna.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:King%27s_Gate_Suomenlinna.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    culture:[{name:"サウナ(ラヤポルッティ、現存最古の公衆サウナ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Rajaportin%20sauna.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Rajaportin_sauna.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    food:[{name:"シナモンロール(コルヴァプースティ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Korvapuusti.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Korvapuusti.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    facility:[{name:"テンペリアウキオ教会(岩の教会)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Temppeliaukio%20Church%20Helsinki.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Temppeliaukio_Church_Helsinki.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    nature:[{name:"湖と山並みの風景(ラップランド)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Lapland%20Finland%20landscape.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Lapland_Finland_landscape.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    symbol:[{name:"オオハクチョウ(国鳥)のつがい",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Whooper%20swans%20Finland.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Whooper_swans_Finland.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}]
+  }
+});
