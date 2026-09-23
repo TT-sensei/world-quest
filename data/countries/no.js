0c30ef0e37a1a6237a028b1202434181658fb9df
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 12:42:17 +0900

    noを国図鑑に追加

---
+++ data/countries/no.js
@@ -0,0 +1,20 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"no",
+  name:"ノルウェー",
+  code:"NO",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"オスロ",
+  flag:"assets/flags/no.svg",
+  emoji:"🇳🇴",
+  items:{
+    place:[{name:"フィヨルドの絶景(ガイランゲル)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Geirangerfjord%20from%20Flydalsjuvet%2C%20Norway.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Geirangerfjord_from_Flydalsjuvet,_Norway.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    heritage:[{name:"ブリッゲン地区の木造倉庫群(ベルゲン)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bryggen%20in%20Bergen.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Bryggen_in_Bergen.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    culture:[{name:"サーミの伝統衣装(ガクティ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Sami%20traditional%20clothing.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Sami_traditional_clothing.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    food:[{name:"サーモンのグリル",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Grilled%20salmon.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Grilled_salmon.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    facility:[{name:"オスロ・オペラハウス",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Oslo%20Opera%20House.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Oslo_Opera_House.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    nature:[{name:"オーロラ(トロムソ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Aurora%20Borealis%20over%20Tromso.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Aurora_Borealis_over_Tromso.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    symbol:[{name:"トナカイ",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Reindeer%20in%20Norway.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Reindeer_in_Norway.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}]
+  }
+});
