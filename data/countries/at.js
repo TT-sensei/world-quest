b3b111b2bf895c85cdd473fe0c6cea05ef18017a
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 17:28:20 +0900

    Add Austria to WORLD QUEST

---
+++ data/countries/at.js
@@ -0,0 +1,69 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"at",
+  name:"オーストリア",
+  code:"AT",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"ウィーン",
+  flag:"assets/flags/at.svg",
+  emoji:"🇦🇹",
+  items:{
+    place:[{
+      name:"シュテファン広場(ウィーン旧市街)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Wien%20-%20Stephansplatz%20-%20View%20SE%20%26%20Up%20on%20Stephansdom%20-%20Nordturm%2068%20m%20high.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Wien_-_Stephansplatz_-_View_SE_%26_Up_on_Stephansdom_-_Nordturm_68_m_high.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Txllxt TxllxT"
+    }],
+    heritage:[{
+      name:"シェーンブルン宮殿",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Wien%20-%20Schloss%20Sch%C3%B6nbrunn.JPG?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Wien_-_Schloss_Sch%C3%B6nbrunn.JPG",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"C.Stadler/Bwag"
+    }],
+    culture:[{
+      name:"国立歌劇場の客席(音楽文化)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Wien%20-%20Staatsoper%2C%20Zuschauerraum.JPG?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Wien_-_Staatsoper,_Zuschauerraum.JPG",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"C.Stadler/Bwag"
+    }],
+    food:[{
+      name:"ザッハトルテ",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Sachertorte%20DSC03027%20retouched.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Sachertorte_DSC03027_retouched.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 2.0",
+      credit:"David Monniaux / AlMare"
+    }],
+    facility:[{
+      name:"ハルシュタットの街並み",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Hallstatt%20-%20Gesamtansicht.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Hallstatt_-_Gesamtansicht.jpg",
+      source:"Wikimedia Commons",
+      license:"FAL(自由芸術ライセンス)",
+      credit:"Taxiarchos228"
+    }],
+    nature:[{
+      name:"グロースグロックナー(アルプス最高峰の一つ)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Grossglockner%20from%20SW.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Grossglockner_from_SW.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 2.0 Austria",
+      credit:"Michael Schmid"
+    }],
+    symbol:[{
+      name:"国章の鷲(連邦の紋章)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Austria%20Bundesadler.svg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Austria_Bundesadler.svg",
+      source:"Wikimedia Commons",
+      license:"パブリックドメイン",
+      credit:"Peter Diem / Austrian BMLV; SVG: Great Brightstar"
+    }]
+  }
+});
