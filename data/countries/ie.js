270d038ad39d29b3fc6ea5f5a2669bf1ee6e0e9d
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 12:42:31 +0900

    ieを国図鑑に追加

---
+++ data/countries/ie.js
@@ -0,0 +1,20 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"ie",
+  name:"アイルランド",
+  code:"IE",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"ダブリン",
+  flag:"assets/flags/ie.svg",
+  emoji:"🇮🇪",
+  items:{
+    place:[{name:"ヘイペニー橋(ダブリンの街並み)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Ha%27penny%20Bridge%20Dublin.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Ha%27penny_Bridge_Dublin.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    heritage:[{name:"ニューグレンジの入口石",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Newgrange%20entrance%20stone.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Newgrange_entrance_stone.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    culture:[{name:"アイリッシュダンス",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Irish%20dance.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Irish_dance.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    food:[{name:"アイリッシュシチュー",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Irish%20stew.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Irish_stew.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    facility:[{name:"トリニティ・カレッジ図書館(ロングルーム)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Long%20Room%20Interior%2C%20Trinity%20College%20Dublin%2C%20Ireland.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Long_Room_Interior,_Trinity_College_Dublin,_Ireland.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    nature:[{name:"モハーの断崖",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Cliffs%20of%20Moher.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Cliffs_of_Moher.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}],
+    symbol:[{name:"シャムロック(三つ葉のクローバー)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Shamrock.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Shamrock.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wikimedia Commons contributor"}]
+  }
+});
