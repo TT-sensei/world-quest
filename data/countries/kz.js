31856fa6d0ed5e5f0d876c3ab99a96018428cfbb
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 17:55:22 +0900

    Add Kazakhstan to WORLD QUEST

---
+++ data/countries/kz.js
@@ -0,0 +1,69 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"kz",
+  name:"カザフスタン",
+  code:"KZ",
+  region:"アジア",
+  continent:"ユーラシア",
+  capital:"アスタナ",
+  flag:"assets/flags/kz.svg",
+  emoji:"🇰🇿",
+  items:{
+    place:[{
+      name:"丘から見た街並み(アルマトイ)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/View%20of%20Almaty%20from%20the%20hills.png?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:View_of_Almaty_from_the_hills.png",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"ShadowNinja1080"
+    }],
+    heritage:[{
+      name:"ホージャ・アフマド・ヤサヴィー廟(トルキスタン)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Mausoleum%20of%20Khoja%20Ahmed%20Yasawi%20in%20Hazrat-e%20Turkestan%2C%20Kazakhstan.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Mausoleum_of_Khoja_Ahmed_Yasawi_in_Hazrat-e_Turkestan,_Kazakhstan.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Petar Milošević"
+    }],
+    culture:[{
+      name:"カザフの伝統衣装",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Kazakh%20traditional%20clothing.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Kazakh_traditional_clothing.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Apluswiki"
+    }],
+    food:[{
+      name:"ベシュバルマク(茹で肉と麺の料理)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Beshbarmak%2C%20national%20dish%20(3991850909).jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Beshbarmak,_national_dish_(3991850909).jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 2.0",
+      credit:"Peretz Partensky"
+    }],
+    facility:[{
+      name:"バイテレク・タワー(アスタナ)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Baiterek.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Baiterek.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"אמר מר"
+    }],
+    nature:[{
+      name:"チャリンキャニオン",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Charyn%20Canyon%2C%20Kazakhstan%2002.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Charyn_Canyon,_Kazakhstan_02.jpg",
+      source:"Wikimedia Commons",
+      license:"CC0 1.0",
+      credit:"Bgag"
+    }],
+    symbol:[{
+      name:"黄金人間(アルティン・アダム)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Golden%20Man%20in%20Central%20State%20Museum%20of%20the%20Republic%20of%20Kazakhstan.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Golden_Man_in_Central_State_Museum_of_the_Republic_of_Kazakhstan.jpg",
+      source:"Wikimedia Commons",
+      license:"パブリックドメイン",
+      credit:"Interfase"
+    }]
+  }
+});
