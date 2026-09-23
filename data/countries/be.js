698c4c32a7aea7936bb823714549e098a206ccdc
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 12:20:47 +0900

    Add Belgium country data

---
+++ data/countries/be.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"be",
+  name:"ベルギー",
+  code:"BE",
+  region:"ヨーロッパ",
+  continent:"ユーラシア",
+  capital:"ブリュッセル",
+  flag:"assets/flags/be.svg",
+  emoji:"🇧🇪",
+  items:{
+    place:[
+      {
+        name:"グラン＝プラス",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Grand%20Place%2C%20Brussels%2C%20Belgium.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Grand_Place,_Brussels,_Belgium.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Wikimedia Commons(Wiki Loves Public Space 2017)"
+      }
+    ],
+    heritage:[
+      {
+        name:"ブルージュ歴史地区",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bruges%20Belgium.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Bruges_Belgium.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Wikimedia Commons contributor"
+      }
+    ],
+    culture:[
+      {
+        name:"ベルギー漫画センター(旧ワウクェズ百貨店)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Centre%20belge%20de%20la%20BD%202017.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Centre_belge_de_la_BD_2017.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA",
+        credit:"Wikimedia Commons contributor"
+      }
+    ],
+    food:[
+      {
+        name:"ワッフル",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Brusselse%20waffel.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Brusselse_waffel.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Wikimedia Commons contributor"
+      }
+    ],
+    facility:[
+      {
+        name:"アトミウム",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Atomium%2C%20Brussels%2C%20Belgium.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Atomium,_Brussels,_Belgium.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Wikimedia Commons(Wiki Loves Public Space 2017)"
+      }
+    ],
+    nature:[
+      {
+        name:"アルデンヌの森",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Vue%20foret%20ardenne%20belge.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Vue_foret_ardenne_belge.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA",
+        credit:"Wikimedia Commons contributor"
+      }
+    ],
+    symbol:[
+      {
+        name:"小便小僧",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bruxelles%20Manneken%20Pis%20cropped.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Bruxelles_Manneken_Pis_cropped.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Myrabella"
+      }
+    ]
+  }
+});
