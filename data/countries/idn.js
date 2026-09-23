0d1854f38a41b5903f96557ed14af0a967306b4f
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 17:34:41 +0900

    Add Indonesia to WORLD QUEST

---
+++ data/countries/idn.js
@@ -0,0 +1,69 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"idn",
+  name:"インドネシア",
+  code:"ID",
+  region:"アジア",
+  continent:"ユーラシア",
+  capital:"ジャカルタ",
+  flag:"assets/flags/id.svg",
+  emoji:"🇮🇩",
+  items:{
+    place:[{
+      name:"ライステラス(バリ島)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Rice%20terraces%20on%20Bali%20-%20Tegalalang%20Rice%20Terrace%20-%20Indonesia%2005.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Rice_terraces_on_Bali_-_Tegalalang_Rice_Terrace_-_Indonesia_05.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Thomas Fuhrmann"
+    }],
+    heritage:[{
+      name:"ボロブドゥール遺跡",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Borobudur%20temple%20from%20above.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Borobudur_temple_from_above.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Maulana Yusuf"
+    }],
+    culture:[{
+      name:"バティック(ろうけつ染め模様)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Batik%20pattern%20-%20kawung.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Batik_pattern_-_kawung.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 3.0",
+      credit:"Alteaven"
+    }],
+    food:[{
+      name:"ナシゴレン(焼き飯)の朝食セット",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Nasi%20Goreng%20Breakfast%20Set%20in%20Solo.JPG?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Nasi_Goreng_Breakfast_Set_in_Solo.JPG",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 3.0",
+      credit:"Gunawan Kartapranata"
+    }],
+    facility:[{
+      name:"イスティクラル・モスクの礼拝堂",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Istiqlal%20Mosque%20Jakarta%20prayer%20hall%20interior%20view%2001.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Istiqlal_Mosque_Jakarta_prayer_hall_interior_view_01.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Aisha Tanduk"
+    }],
+    nature:[{
+      name:"パダール島のピンクビーチ(コモド国立公園)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Pink%20Beach%2C%20Padar%20Island%2C%20Komodo%20National%20Park.jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Pink_Beach,_Padar_Island,_Komodo_National_Park.jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Devagonal"
+    }],
+    symbol:[{
+      name:"コモドオオトカゲ(国立公園に生息する固有種)",
+      image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Komodo%20dragon%20(Varanus%20komodoensis).jpg?width=1000",
+      sourceImage:"https://commons.wikimedia.org/wiki/File:Komodo_dragon_(Varanus_komodoensis).jpg",
+      source:"Wikimedia Commons",
+      license:"CC BY-SA 4.0",
+      credit:"Charles J. Sharp"
+    }]
+  }
+});
