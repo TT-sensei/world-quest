4cc3ae113eac75fc8e96bf6e64646d1d4104ac91
TT-sensei <rlrieaeoy@gmail.com>
2026-09-25 05:35:23 +0900

    Add Costa Rica country data

---
+++ data/countries/cr.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"cr",name:"コスタリカ",code:"CR",region:"北アメリカ",continent:"北アメリカ",capital:"サンホセ",
+  flag:"assets/flags/cr.svg",emoji:"🇨🇷",
+  items:{
+    place:[{name:"プエルト・ビエホ(カリブ海沿岸の町)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Puerto%20Viejo.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Puerto_Viejo.jpg",source:"Wikimedia Commons",license:"GFDL 1.2+ / CC BY-SA 3.0",credit:"Haakon S. Krohn"}],
+    heritage:[{name:"ディキスの石球(現地遺跡)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Diquis%20Stone%20Spheres%20in%20situ%20Finca%206%20CR%2012%202020.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Diquis_Stone_Spheres_in_situ_Finca_6_CR_12_2020.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Diego Padilla Durán y Mariordo"}],
+    culture:[{name:"彩色された牛車(カレータ)の車輪",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Rueda%20de%20Carreta%20T%C3%ADpica%20Costa%20Rica.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Rueda_de_Carreta_T%C3%ADpica_Costa_Rica.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Jmillan325"}],
+    food:[{name:"ガジョ・ピント(米と豆の朝食料理)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Costa%20Rican%20Gallo%20Pinto.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Costa_Rican_Gallo_Pinto.jpg",source:"Wikimedia Commons",license:"CC BY 3.0",credit:"Larry" }],
+    facility:[{name:"国立劇場(サンホセ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/National%20Theatre%20of%20Costa%20Rica.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:National_Theatre_of_Costa_Rica.jpg",source:"Wikimedia Commons",license:"CC BY 2.0",credit:"Richie Diesterheft"}],
+    nature:[{name:"アレナル火山",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Arenal%20volcano%20(70785p)%20(cropped).jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Arenal_volcano_(70785p)_(cropped).jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Rhododendrites"}],
+    symbol:[{name:"ケツァール(モンテベルデ雲霧林)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Pharomachrus%20mocinno%20Monteverde%2001.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Pharomachrus_mocinno_Monteverde_01.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Cephas"}]
+  }
+});
