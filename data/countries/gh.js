f208eac3867e45b1b1a6f73ce6105f84d65abb28
TT-sensei <rlrieaeoy@gmail.com>
2026-09-24 19:00:26 +0900

    Add Ghana country data

---
+++ data/countries/gh.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"gh",name:"ガーナ",code:"GH",region:"アフリカ",continent:"アフリカ",capital:"アクラ",
+  flag:"assets/flags/gh.svg",emoji:"🇬🇭",
+  items:{
+    place:[{name:"独立記念アーチ(アクラ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Independence%20Arch%20Accra%20Ghana.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Independence_Arch_Accra_Ghana.jpg",source:"Wikimedia Commons",license:"CC BY 2.0",credit:"Flowizm"}],
+    heritage:[{name:"ケープコースト城",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Cape%20Coast%20castle.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Cape_Coast_castle.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Kodex"}],
+    culture:[{name:"ケンテ布を織る様子",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Weaving%20of%20kente%20cloth.JPG?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Weaving_of_kente_cloth.JPG",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Abbeyfamily"}],
+    food:[{name:"ジョロフライス",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Jollof%20Rice%20Ghana.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Jollof_Rice_Ghana.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Fquasie"}],
+    facility:[{name:"国立劇場(アクラ)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/National%20Theatre%20of%20Ghana%2020060826.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:National_Theatre_of_Ghana_20060826.jpg",source:"Wikimedia Commons",license:"CC BY-SA 2.0",credit:"Guido Sohne"}],
+    nature:[{name:"カクム国立公園のキャノピーウォーク",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Kakum%20Canopy%20Walk%2C%20Kakum%20National%20Park%2C%20Ghana.JPG?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Kakum_Canopy_Walk,_Kakum_National_Park,_Ghana.JPG",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Benggriff"}],
+    symbol:[{name:"サンコファ(アディンクラ文様)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Sankofa%2C%20simbolo%20adinkra.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Sankofa,_simbolo_adinkra.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"AlexandreLuizOliveira"}]
+  }
+});
