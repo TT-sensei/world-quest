2f63a1d01d968e25f67592df14ce18d8c8a4ed0e
TT-sensei <rlrieaeoy@gmail.com>
2026-09-25 05:37:15 +0900

    Add Cameroon country data

---
+++ data/countries/cm.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"cm",name:"カメルーン",code:"CM",region:"アフリカ",continent:"アフリカ",capital:"ヤウンデ",
+  flag:"assets/flags/cm.svg",emoji:"🇨🇲",
+  items:{
+    place:[{name:"ヤウンデ市立湖",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Lac%20Municipal%20de%20Yaound%C3%A9.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Lac_Municipal_de_Yaound%C3%A9.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Kondah"}],
+    heritage:[{name:"ジャ動物保護区の熱帯雨林",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Dja%20Faunal%20Reserve-109438.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Dja_Faunal_Reserve-109438.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0-IGO",credit:"C. Hance / UNESCO"}],
+    culture:[{name:"バミレケの伝統舞踊",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bamileke%20dancers.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Bamileke_dancers.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Anya Lothrop"}],
+    food:[{name:"ンドレ(ピーナッツと葉野菜の煮込み)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Ndol%C3%A9%20camerounais.JPG?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Ndol%C3%A9_camerounais.JPG",source:"Wikimedia Commons",license:"CC BY 3.0",credit:"PRA"}],
+    facility:[{name:"バムン王宮(フンバン)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bamun%20sultan%20palace.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Bamun_sultan_palace.jpg",source:"Wikimedia Commons",license:"CC BY-SA 2.5 / GFDL 1.2+",credit:"Amcaja"}],
+    nature:[{name:"ルムシキの奇岩(カプシキ山)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Northern%20Cameroon%20banner%20Rhumsiki%20with%20Kapsiki%20Peak.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Northern_Cameroon_banner_Rhumsiki_with_Kapsiki_Peak.jpg",source:"Wikimedia Commons",license:"CC BY 2.0",credit:"Alfred Weidinger"}],
+    symbol:[{name:"ライオン(アフリカの代表的な動物)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Lion%20d%27Afrique.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Lion_d%27Afrique.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Clément Bardot"}]
+  }
+});
