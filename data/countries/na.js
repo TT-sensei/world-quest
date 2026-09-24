e5cd6fb9222d7f0997c3b2520980d465f931a33d
TT-sensei <rlrieaeoy@gmail.com>
2026-09-25 05:15:00 +0900

    Add Namibia country data

---
+++ data/countries/na.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"na",name:"ナミビア",code:"NA",region:"アフリカ",continent:"アフリカ",capital:"ウィントフック",
+  flag:"assets/flags/na.svg",emoji:"🇳🇦",
+  items:{
+    place:[{name:"ジェティと灯台(スワコプムント)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Mole%2C%20Jetty%20and%20Lighthouse%20Swakopmund%2C%20Namibia.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Mole,_Jetty_and_Lighthouse_Swakopmund,_Namibia.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Olga Ernst"}],
+    heritage:[{name:"キリンの岩絵(トゥヴェイフルフォンテーン)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Giraffe%2C%20Twyfelfontein.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Giraffe,_Twyfelfontein.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0",credit:"Schnobby"}],
+    culture:[{name:"ヘレロ族の伝統衣装",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Herero%20ladies.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Herero_ladies.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Hans Hillewaert"}],
+    food:[{name:"ビルトング(干し肉)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Biltong%20from%20Namibia.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Biltong_from_Namibia.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Aisha.shipanga"}],
+    facility:[{name:"ヴェルマン館(スワコプムント)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Swakopmund%20-%20Woermannhaus.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Swakopmund_-_Woermannhaus.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Lidine Mia"}],
+    nature:[{name:"ソッサスフレイの赤い砂丘",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Duna%20en%20Sossusvlei%2C%20Namibia%2C%202018-08-06%2C%20DD%20002.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Duna_en_Sossusvlei,_Namibia,_2018-08-06,_DD_002.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Diego Delso"}],
+    symbol:[{name:"オリックス(エトーシャ国立公園)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Oryx%20gazella%20-Etosha%20National%20Park%2C%20Namibia-8.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Oryx_gazella_-Etosha_National_Park,_Namibia-8.jpg",source:"Wikimedia Commons",license:"CC BY-SA 2.0",credit:"Joachim Huber"}]
+  }
+});
