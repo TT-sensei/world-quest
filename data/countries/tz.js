fc9353cb28647c1dad3bd4f1189f74e3aeb95f3d
TT-sensei <rlrieaeoy@gmail.com>
2026-09-24 19:06:02 +0900

    Add Tanzania country data

---
+++ data/countries/tz.js
@@ -0,0 +1,14 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"tz",name:"タンザニア",code:"TZ",region:"アフリカ",continent:"アフリカ",capital:"ドドマ",
+  flag:"assets/flags/tz.svg",emoji:"🇹🇿",
+  items:{
+    place:[{name:"ストーンタウンの路地(ザンジバル)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Streets%20of%20Stone%20town%2C%20Zanzibar.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Streets_of_Stone_town,_Zanzibar.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Awadh Abdul"}],
+    heritage:[{name:"ザンジバルのストーンタウン",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Stone%20Town%20of%20Zanzibar-108843.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Stone_Town_of_Zanzibar-108843.jpg",source:"Wikimedia Commons",license:"CC BY-SA 3.0-IGO",credit:"Ron Van Oers"}],
+    culture:[{name:"マサイの伝統的なジャンプダンス",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Masaidance.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Masaidance.jpg",source:"Wikimedia Commons",license:"CC BY 3.0",credit:"Dmitri Markine"}],
+    food:[{name:"ウガリ(東アフリカの主食)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Tanzania%20Food%20Ugali%20and%20Mlenda.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Tanzania_Food_Ugali_and_Mlenda.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"MWANI"}],
+    facility:[{name:"オールド・フォート(ザンジバル)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Old%20Fort%20of%20Zanzibar%20in%20Stone%20town%20area.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Old_Fort_of_Zanzibar_in_Stone_town_area.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Erasmus Kamugisha"}],
+    nature:[{name:"キリマンジャロ山",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Kilimanjaro%20Tanzania.jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Kilimanjaro_Tanzania.jpg",source:"Wikimedia Commons",license:"CC BY-SA 4.0",credit:"Wvuijk"}],
+    symbol:[{name:"マサイキリン(セレンゲティ国立公園)",image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Masai%20Giraffe%2C%20Serengeti%20National%20Park%2C%20Tanzania%20(2010).jpg?width=1000",sourceImage:"https://commons.wikimedia.org/wiki/File:Masai_Giraffe,_Serengeti_National_Park,_Tanzania_(2010).jpg",source:"Wikimedia Commons",license:"CC BY 3.0",credit:"Sajjad Sherally Fazel"}]
+  }
+});
