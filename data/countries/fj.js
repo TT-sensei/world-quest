a265c37963bf3514a7aaa8d10f886b267aa1bc90
TT-sensei <rlrieaeoy@gmail.com>
2026-09-25 15:42:38 +0900

    Add Fiji country data

---
+++ data/countries/fj.js
@@ -0,0 +1,83 @@
+window.WORLD_COUNTRIES=window.WORLD_COUNTRIES||[];
+window.WORLD_COUNTRIES.push({
+  id:"fj",
+  name:"フィジー",
+  code:"FJ",
+  region:"オセアニア",
+  continent:"オーストラリア",
+  capital:"スバ",
+  flag:"assets/flags/fj.svg",
+  emoji:"🇫🇯",
+  items:{
+    place:[
+      {
+        name:"ヴィティレヴ島のコーラルコースト",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/2019-01-28%20Viti%20Levu%27s%20Coral%20coast%2C%20Fiji.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:2019-01-28_Viti_Levu%27s_Coral_coast,_Fiji.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 4.0",
+        credit:"Maksym Kozlenko"
+      }
+    ],
+    heritage:[
+      {
+        name:"レブカの歴史的な通り並み",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Levuka%20banner%20Street.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Levuka_banner_Street.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 2.5",
+        credit:"Merbabu"
+      }
+    ],
+    culture:[
+      {
+        name:"フィジーの伝統舞踊(メケ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Fiji%20Meke-Meke%20(National%20Dance)%20(NBY%20443841).jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Fiji_Meke-Meke_(National_Dance)_(NBY_443841).jpg",
+        source:"Wikimedia Commons",
+        license:"Public domain",
+        credit:"Unknown author"
+      }
+    ],
+    food:[
+      {
+        name:"ココナッツミルクとライムでマリネした生魚料理(ココダ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Kokodafood.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Kokodafood.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY 2.0",
+        credit:"Yuko Hasegawa"
+      }
+    ],
+    facility:[
+      {
+        name:"聖心教会(レブカ)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/ChurchLevuka.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:ChurchLevuka.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 2.5",
+        credit:"Anton Leddin"
+      }
+    ],
+    nature:[
+      {
+        name:"ボウマの滝(タベウニ島)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/BoumaFalls.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:BoumaFalls.jpg",
+        source:"Wikimedia Commons",
+        license:"CC BY-SA 3.0",
+        credit:"Doron"
+      }
+    ],
+    symbol:[
+      {
+        name:"タギモウチアの花(フィジー固有種)",
+        image:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Tagimoucia%20plant%20in%20Taveuni.jpg?width=1000",
+        sourceImage:"https://commons.wikimedia.org/wiki/File:Tagimoucia_plant_in_Taveuni.jpg",
+        source:"Wikimedia Commons",
+        license:"Public domain",
+        credit:"US Embassy Suva"
+      }
+    ]
+  }
+});
